fa9be3fed68f5c89d909383227932684051e3c27
Jason Wedderburn <jasonwj96@gmail.com>
2024-11-10 02:26:59 -0500

    added sprites

---
+++ RescateMarino.Designer.vb
@@ -0,0 +1,190 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class RescateMarino
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        PictureBox1 = New PictureBox()
+        PictureBox2 = New PictureBox()
+        PictureBox3 = New PictureBox()
+        PictureBox4 = New PictureBox()
+        PictureBox5 = New PictureBox()
+        PictureBox6 = New PictureBox()
+        PictureBox7 = New PictureBox()
+        PictureBox8 = New PictureBox()
+        PictureBox9 = New PictureBox()
+        CType(PictureBox1, ComponentModel.ISupportInitialize).BeginInit()
+        CType(PictureBox2, ComponentModel.ISupportInitialize).BeginInit()
+        CType(PictureBox3, ComponentModel.ISupportInitialize).BeginInit()
+        CType(PictureBox4, ComponentModel.ISupportInitialize).BeginInit()
+        CType(PictureBox5, ComponentModel.ISupportInitialize).BeginInit()
+        CType(PictureBox6, ComponentModel.ISupportInitialize).BeginInit()
+        CType(PictureBox7, ComponentModel.ISupportInitialize).BeginInit()
+        CType(PictureBox8, ComponentModel.ISupportInitialize).BeginInit()
+        CType(PictureBox9, ComponentModel.ISupportInitialize).BeginInit()
+        SuspendLayout()
+        ' 
+        ' PictureBox1
+        ' 
+        PictureBox1.BackColor = Color.Transparent
+        PictureBox1.Image = My.Resources.Resources.fuel_sprite
+        PictureBox1.Location = New Point(23, 12)
+        PictureBox1.Name = "PictureBox1"
+        PictureBox1.Size = New Size(80, 80)
+        PictureBox1.SizeMode = PictureBoxSizeMode.Zoom
+        PictureBox1.TabIndex = 0
+        PictureBox1.TabStop = False
+        ' 
+        ' PictureBox2
+        ' 
+        PictureBox2.Anchor = AnchorStyles.Top Or AnchorStyles.Right
+        PictureBox2.BackColor = Color.Transparent
+        PictureBox2.Image = My.Resources.Resources.heart_sprite
+        PictureBox2.Location = New Point(1729, 12)
+        PictureBox2.Name = "PictureBox2"
+        PictureBox2.Size = New Size(47, 49)
+        PictureBox2.SizeMode = PictureBoxSizeMode.Zoom
+        PictureBox2.TabIndex = 1
+        PictureBox2.TabStop = False
+        ' 
+        ' PictureBox3
+        ' 
+        PictureBox3.Anchor = AnchorStyles.Top Or AnchorStyles.Right
+        PictureBox3.BackColor = Color.Transparent
+        PictureBox3.Image = My.Resources.Resources.heart_sprite
+        PictureBox3.Location = New Point(1676, 12)
+        PictureBox3.Name = "PictureBox3"
+        PictureBox3.Size = New Size(47, 49)
+        PictureBox3.SizeMode = PictureBoxSizeMode.Zoom
+        PictureBox3.TabIndex = 2
+        PictureBox3.TabStop = False
+        ' 
+        ' PictureBox4
+        ' 
+        PictureBox4.Anchor = AnchorStyles.Top Or AnchorStyles.Right
+        PictureBox4.BackColor = Color.Transparent
+        PictureBox4.Image = My.Resources.Resources.heart_sprite
+        PictureBox4.Location = New Point(1570, 12)
+        PictureBox4.Name = "PictureBox4"
+        PictureBox4.Size = New Size(47, 49)
+        PictureBox4.SizeMode = PictureBoxSizeMode.Zoom
+        PictureBox4.TabIndex = 4
+        PictureBox4.TabStop = False
+        ' 
+        ' PictureBox5
+        ' 
+        PictureBox5.Anchor = AnchorStyles.Top Or AnchorStyles.Right
+        PictureBox5.BackColor = Color.Transparent
+        PictureBox5.Image = My.Resources.Resources.heart_sprite
+        PictureBox5.Location = New Point(1623, 12)
+        PictureBox5.Name = "PictureBox5"
+        PictureBox5.Size = New Size(47, 49)
+        PictureBox5.SizeMode = PictureBoxSizeMode.Zoom
+        PictureBox5.TabIndex = 3
+        PictureBox5.TabStop = False
+        ' 
+        ' PictureBox6
+        ' 
+        PictureBox6.Anchor = AnchorStyles.Top Or AnchorStyles.Right
+        PictureBox6.BackColor = Color.Transparent
+        PictureBox6.Image = My.Resources.Resources.heart_sprite
+        PictureBox6.Location = New Point(1517, 12)
+        PictureBox6.Name = "PictureBox6"
+        PictureBox6.Size = New Size(47, 49)
+        PictureBox6.SizeMode = PictureBoxSizeMode.Zoom
+        PictureBox6.TabIndex = 5
+        PictureBox6.TabStop = False
+        ' 
+        ' PictureBox7
+        ' 
+        PictureBox7.BackColor = Color.Transparent
+        PictureBox7.Image = My.Resources.Resources.shark_sprite_3
+        PictureBox7.Location = New Point(867, 801)
+        PictureBox7.Name = "PictureBox7"
+        PictureBox7.Size = New Size(101, 91)
+        PictureBox7.SizeMode = PictureBoxSizeMode.Zoom
+        PictureBox7.TabIndex = 6
+        PictureBox7.TabStop = False
+        ' 
+        ' PictureBox8
+        ' 
+        PictureBox8.BackColor = Color.Transparent
+        PictureBox8.Image = My.Resources.Resources.lifeboat_sprite1
+        PictureBox8.Location = New Point(755, 662)
+        PictureBox8.Name = "PictureBox8"
+        PictureBox8.Size = New Size(166, 133)
+        PictureBox8.SizeMode = PictureBoxSizeMode.Zoom
+        PictureBox8.TabIndex = 7
+        PictureBox8.TabStop = False
+        ' 
+        ' PictureBox9
+        ' 
+        PictureBox9.Anchor = AnchorStyles.Right
+        PictureBox9.BackColor = Color.Transparent
+        PictureBox9.Image = My.Resources.Resources.lifeboar_sprite_21
+        PictureBox9.Location = New Point(1553, 12)
+        PictureBox9.Name = "PictureBox9"
+        PictureBox9.Size = New Size(170, 350)
+        PictureBox9.SizeMode = PictureBoxSizeMode.Zoom
+        PictureBox9.TabIndex = 8
+        PictureBox9.TabStop = False
+        ' 
+        ' RescateMarino
+        ' 
+        AutoScaleDimensions = New SizeF(10F, 25F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackColor = Color.White
+        BackgroundImage = My.Resources.Resources.sea_sprite_41
+        ClientSize = New Size(1778, 1244)
+        Controls.Add(PictureBox8)
+        Controls.Add(PictureBox7)
+        Controls.Add(PictureBox6)
+        Controls.Add(PictureBox4)
+        Controls.Add(PictureBox5)
+        Controls.Add(PictureBox3)
+        Controls.Add(PictureBox2)
+        Controls.Add(PictureBox1)
+        Controls.Add(PictureBox9)
+        Name = "RescateMarino"
+        Text = "Rescate Marino"
+        CType(PictureBox1, ComponentModel.ISupportInitialize).EndInit()
+        CType(PictureBox2, ComponentModel.ISupportInitialize).EndInit()
+        CType(PictureBox3, ComponentModel.ISupportInitialize).EndInit()
+        CType(PictureBox4, ComponentModel.ISupportInitialize).EndInit()
+        CType(PictureBox5, ComponentModel.ISupportInitialize).EndInit()
+        CType(PictureBox6, ComponentModel.ISupportInitialize).EndInit()
+        CType(PictureBox7, ComponentModel.ISupportInitialize).EndInit()
+        CType(PictureBox8, ComponentModel.ISupportInitialize).EndInit()
+        CType(PictureBox9, ComponentModel.ISupportInitialize).EndInit()
+        ResumeLayout(False)
+    End Sub
+
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents PictureBox2 As PictureBox
+    Friend WithEvents PictureBox3 As PictureBox
+    Friend WithEvents PictureBox4 As PictureBox
+    Friend WithEvents PictureBox5 As PictureBox
+    Friend WithEvents PictureBox6 As PictureBox
+    Friend WithEvents PictureBox7 As PictureBox
+    Friend WithEvents PictureBox8 As PictureBox
+    Friend WithEvents PictureBox9 As PictureBox
+
+End Class
---
+++ RescateMarino.vb
@@ -0,0 +1,7 @@
+﻿Public Class RescateMarino
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Me.Size = New Size(1800, 1300)
+        Me.FormBorderStyle = FormBorderStyle.FixedDialog
+        Me.MaximizeBox = False
+    End Sub
+End Class
